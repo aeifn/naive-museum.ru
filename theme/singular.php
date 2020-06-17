fe501ec8c8e880b75a440b5136e9f80f5ed55ed3
Egor Kuzmichev <egor@math.ru>
2020-06-17 11:50:21 +0300

    Update singular.php

--- theme/singular.php
+++ theme/singular.php
@@ -11,26 +11,14 @@
 
 get_header();
 ?>
-<?php get_template_part( 'template-parts/submenu' ); ?>
-
-
 <main id="site-content" role="main">
-
 	<?php
-
 	if ( have_posts() ) {
-
 		while ( have_posts() ) {
 			the_post();
-
 			get_template_part( 'template-parts/content', get_post_type() );
 		}
 	}
-
 	?>
-
 </main><!-- #site-content -->
-
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
-
 <?php get_footer(); ?>
