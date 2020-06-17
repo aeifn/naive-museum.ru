d941db76274c2623b4f15e9d95607bb5364497f0
Egor Kuzmichev <egor@math.ru>
2020-06-17 11:15:08 +0300

    Update archive.php

--- theme/archive.php
+++ theme/archive.php
@@ -1,37 +1,21 @@
 <?php get_header() ; ?>
-<?php get_template_part ( 'template-parts/entry-header' ); ?>
-  <div class="container">
-
-
-
+  <div class="container mt-4">
     <div class="row">
-
       <div class="col-md-4">
-        <?php get_template_part( 'template-parts/sidebar' ); ?>
+        <?php get_sidebar(); ?>
       </div>
-
       <div class="col-md-8">
-
-        <!-- Меню подразделения -->
-        <div class="row">
-          <div class="col-12">
-          <?php get_template_part( 'template-parts/submenu' ); ?>
-          </div>
-        </div>
-
         <!-- Плитка -->
-        <div class="row">
-          <?php while ( have_posts() ): ?>
-            <div class="col-md-6 d-flex mt-3">
+        <div class="row ">
+          <?php while (have_posts()): ?>
+            <div class="col-md-6 d-flex">
               <?php the_post();
-              get_template_part( 'template-parts/card' );
+              get_template_part('template-parts/card');
               ?>
             </div>
           <?php endwhile ?>
         </div>
-
       </div>
     </div>
-
   </div>
 <?php get_footer(); ?>
