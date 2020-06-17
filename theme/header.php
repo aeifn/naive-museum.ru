866dd240274b0c621a96f43d364f14daa5d59a40
Egor Kuzmichev <egor@math.ru>
2020-06-17 11:22:58 +0300

    Update header.php

--- theme/header.php
+++ theme/header.php
@@ -7,43 +7,34 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-
-  <?php
-  wp_body_open();
-  ?>
-
-  <header>
+<?php wp_body_open(); ?>
+<header>
   <div class="container">
-	<div class="row">
-		<div class="col-md-5">
-		<div><a href="http://mos.ru/kultura"><img src="<?= get_template_directory_uri() . '/img/depcult.png' ?>"></a></div>
-		<div>
-      <a href="/"><img src="<?= get_template_directory_uri() . '/img/logo.png' ?>" class="img-fluid"></a>
+    <div class="row">
+        <div class="col-md-5">
+        <div><a href="http://mos.ru/kultura"><img src="<?php echo get_template_directory_uri() . '/img/depcult.png' ?>"></a></div>
+        <div>
+      <a href="/"><img src="<?php echo get_template_directory_uri() . '/img/logo.png' ?>" class="img-fluid"></a>
     </div>
-		</div>
-		<div class="col-md-6 offset-md-1">
-			<!-- Служебные ссылки -->
-			<div class="row">
-			<div class="col-md-6"><a href="#">Summary</a></div>
-			<div class="col-md-6 d-none d-md-block"><a href="#">Версия для слабовидящих</a></div>
-			</div>
-			<!-- Поиск -->
-			<div class="row">
-			SEARCH
-			</div>
-			<!-- Социальные сети -->
-			<div class="row">
-			SOCIAL
-			</div>
-
-		</div>
-	</div>
+        </div>
+        <div class="col-md-6 offset-md-1">
+            <!-- Служебные ссылки -->
+            <div class="row">
+            <div class="col-md-6"><a href="#">Summary</a></div>
+            <div class="col-md-6 d-none d-md-block"><a href="#">Версия для слабовидящих</a></div>
+            </div>
+            <!-- Поиск -->
+            <div class="row">
+            SEARCH
+            </div>
+            <!-- Социальные сети -->
+            <div class="row">
+            <?php get_template_part('template-parts/social-menu'); ?>
+            </div>
+        </div>
+    </div>
 </div>
-
-<?php wp_nav_menu(
-      array(
-    'theme_location' => 'primary'
-  )
-  ); ?>
-
-  </header>
+<?php get_template_part('template-parts/menu'); ?>
+<?php get_template_part('template-parts/entry-header'); ?>
+<?php get_template_part('template-parts/submenu'); ?>
+</header>
