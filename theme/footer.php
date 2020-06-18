6f3790355560fd6c4103e44f235fc1dd7b0a6d37
Egor Kuzmichev <egor@math.ru>
2020-06-18 16:11:46 +0300

    Оформил футер в общем виде

--- theme/footer.php
+++ theme/footer.php
@@ -1,16 +1,19 @@
-<div class="row">
-	<div class="col-md-5">
-		<?php dynamic_sidebar( 'footer-1' ); ?>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5">
+				<?php dynamic_sidebar( 'footer-1' ); ?>
+			</div>
+			<div class="col-md-3">
+				<?php dynamic_sidebar( 'footer-2' ); ?>
+			</div>
+			<div class="col-md-3">
+				<?php dynamic_sidebar( 'footer-3' ); ?>
+			</div>
+		</div>
 	</div>
-	<div class="col-md-3">
-		<?php dynamic_sidebar( 'footer-2' ); ?>
-	</div>
-	<div class="col-md-3">
-		<?php dynamic_sidebar( 'footer-3' ); ?>
-	</div>
-</div>
 
-<?php wp_footer(); ?>
-
+	<?php wp_footer(); ?>
+</footer>
 </body>
 </html>
