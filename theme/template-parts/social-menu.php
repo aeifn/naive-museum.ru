15459ef5cd68a2be4d77225c8cd6ad2da85d10c3
Egor Kuzmichev <egor@math.ru>
2020-06-18 15:25:44 +0300

    Оформил социальное меню

--- theme/template-parts/social-menu.php
+++ theme/template-parts/social-menu.php
@@ -1,18 +1,8 @@
-<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
 		<?php
 		wp_nav_menu(
 			array(
 				'theme_location'  => 'social',
-				'container'       => '',
-				'container_class' => '',
-				'items_wrap'      => '%3$s',
-				'menu_id'         => '',
-				'menu_class'      => '',
 				'depth'           => 1,
-				'link_before'     => '<span class="screen-reader-text">',
-				'link_after'      => '</span>',
-				'fallback_cb'     => '',
 			)
 		);
 		?>
-</ul><!-- .footer-social -->
