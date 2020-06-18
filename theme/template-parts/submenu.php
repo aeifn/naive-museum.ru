9da441250c598d831d081cc9bf3615a57257c090
Egor Kuzmichev <egor@math.ru>
2020-06-18 16:02:04 +0300

    Упростил подменю

--- theme/template-parts/submenu.php
+++ theme/template-parts/submenu.php
@@ -1,10 +1,4 @@
 <?php
-
-    $menu_class='nav nav-pills justify-content-center';
-
  $a=wp_nav_menu(array (
   'start_depth'=> 1,
-//  'container_class' => '',
-  'menu_class'      => $menu_class,
-  'walker' => new WP_Bootstrap_Navwalker(),
 ) ); ?>
