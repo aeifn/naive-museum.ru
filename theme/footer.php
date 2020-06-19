fb91ae4a1eb6e5e705ae4146e4b01f8c78dbf8cf
Andrey Kovalev <kandreys35@gmail.com>
2020-06-19 14:52:20 +0300

    правки меню

--- theme/footer.php
+++ theme/footer.php
@@ -1,13 +1,13 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-5">
+			<div class="col-lg-5 col-md-5">
 				<?php dynamic_sidebar( 'footer-1' ); ?>
 			</div>
-			<div class="col-md-3">
+			<div class="col-lg-3 col-md-7">
 				<?php dynamic_sidebar( 'footer-2' ); ?>
 			</div>
-			<div class="col-md-3">
+			<div class="col-lg-4 col-md-7 offset-md-5">
 				<?php dynamic_sidebar( 'footer-3' ); ?>
 			</div>
 		</div>
