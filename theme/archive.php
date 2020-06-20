c779b42fefac2275a5884ff5f92be6ff3d219909
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-06-21 01:38:26 +0300

    Update archive.php

--- theme/archive.php
+++ theme/archive.php
@@ -1,7 +1,11 @@
-<?php get_header();
+<?php
+get_header();
+get_template_part ( 'template-parts/entry-header' );
+
 if(is_category()) {
   get_template_part( 'template-parts/archive-2' );
 } else {
   get_template_part( 'template-parts/archive-3' );
 }
+
 get_footer();
