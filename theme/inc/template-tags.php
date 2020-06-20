af20200394491c0bed34209719116e5cdc9f5251
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-06-21 01:41:17 +0300

    Create template-tags.php

---
+++ theme/inc/template-tags.php
@@ -0,0 +1,20 @@
+<?php
+function mrlini_site_logo () {
+  $site_title = get_bloginfo( 'name' );
+  ?>
+<div id="logo">
+  <a href="/">
+    <img alt="<?=$site_title?>" src="<?= get_template_directory_uri() . '/img/logo.png' ?>" class="img-fluid">
+  </a>
+</div>
+<?php }
+
+function mrlini_site_description () {
+  $site_description = get_bloginfo( 'description' );
+  ?>
+  <div id="description">
+    <a href="http://mos.ru/kultura">
+      <img src="<?php echo get_template_directory_uri() . '/img/depcult.png' ?>">
+    </a>
+  </div>
+<?php }
