bb53a3f9e94f23c1fb2ad6604c5a8423797cc3dd
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-06-21 01:40:40 +0300

    credits

--- theme/footer.php
+++ theme/footer.php
@@ -1,14 +1,17 @@
+<?php get_template_part( 'template-parts/banners' ); ?>
+
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-5 col-md-5">
+			<div class="col-lg-5">
 				<?php dynamic_sidebar( 'footer-1' ); ?>
 			</div>
-			<div class="col-lg-3 col-md-7">
+			<div class="col-lg-3">
 				<?php dynamic_sidebar( 'footer-2' ); ?>
 			</div>
-			<div class="col-lg-4 col-md-7 offset-md-5">
+			<div class="col-lg-4">
 				<?php dynamic_sidebar( 'footer-3' ); ?>
+				<?php get_template_part( 'template-parts/credits' ); ?>
 			</div>
 		</div>
 	</div>
---
+++ theme/template-parts/credits.php
@@ -0,0 +1,2 @@
+<p id="creator">Сайт сделан Егором&nbsp;Кузьмичевым и Андреем&nbsp;Ковалевым в 2020 году.</p>
+<p id="generator">Работает <a href="https://wordpress.org/">на Вордпрессе</a></p>
