84dfb3f9bc7fed547a73190201c61cc4ec80553d
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-06-23 19:28:07 +0300

    Update footer.php

--- theme/footer.php
+++ theme/footer.php
@@ -1,22 +1,23 @@
-<?php get_template_part( 'template-parts/banners' ); ?>
+<?php get_template_part('template-parts/banners'); ?>
 
 <footer>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5">
-				<?php dynamic_sidebar( 'footer-1' ); ?>
+				<?php dynamic_sidebar('footer-1'); ?>
 			</div>
 			<div class="col-lg-3">
-				<?php dynamic_sidebar( 'footer-2' ); ?>
+				<?php dynamic_sidebar('footer-2'); ?>
 			</div>
 			<div class="col-lg-4">
-				<?php dynamic_sidebar( 'footer-3' ); ?>
-				<?php get_template_part( 'template-parts/credits' ); ?>
+				<?php dynamic_sidebar('footer-3'); ?>
+				<?php get_template_part('template-parts/credits'); ?>
 			</div>
 		</div>
 	</div>
-
-	<?php wp_footer(); ?>
 </footer>
+
+<?php wp_footer(); ?>
+
 </body>
 </html>
