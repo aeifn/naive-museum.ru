e6612ba78b3d2a6974d81bc235c8f546eb00cbe1
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-06-24 16:01:23 +0300

    Пермалинк в карусели

--- theme/template-parts/carousel.php
+++ theme/template-parts/carousel.php
@@ -16,7 +16,7 @@ $posts= get_posts( array ( 'numberposts' => 3 ) );
 <?php foreach($posts as $key=>$post): setup_postdata($post); ?>
       <div class="carousel-item <?php if($key==0) echo 'active' ?>">
         <?php the_post_thumbnail( 'medium_large', array ( 'class' => 'd-block w-100 carousel-image' ) ); ?>
-        <a href="#" class="carousel-caption d-none d-md-block">
+        <a href="<?= the_permalink(); ?>" class="carousel-caption d-none d-md-block">
           <h1><?= the_title(); ?></h1>
           <p><?= the_excerpt(); ?></p>
         </a>
@@ -26,11 +26,11 @@ $posts= get_posts( array ( 'numberposts' => 3 ) );
 
     <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+      <span class="sr-only">Предыдущий</span>
     </a>
     <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+      <span class="sr-only">Следующий</span>
     </a>
 
   </div>
